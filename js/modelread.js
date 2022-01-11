2dab4383e86d7b7804cc5f8c22f9d2e050b5e4fd
minko <695972193@qq.com>
2022-01-11 21:34:04 +0800

    Site updated: 2022-01-11 21:33:57

---
+++ js/modelread.js
@@ -0,0 +1,90 @@
+import * as THREE from './third-party/three/build/three.module.js'
+
+import { GLTFLoader } from './third-party/three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from './third-party/three/examples/jsm/loaders/DRACOLoader.js';
+
+import Stats from './third-party/three/examples/jsm/libs/stats.module.js';
+import { OrbitControls } from './third-party/three/examples/jsm/controls/OrbitControls.js';
+import { RoomEnvironment } from './third-party/three/examples/jsm/environments/RoomEnvironment.js';
+
+
+
+let mixer;
+
+const clock = new THREE.Clock();
+
+const stats = new Stats();
+document.body.appendChild( stats.dom );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputEncoding = THREE.sRGBEncoding;
+document.body.appendChild( renderer.domElement );
+
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xbfe3dd );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+
+const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
+camera.position.set( 5, 2, 8 );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0.5, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/js/third-party/three/examples/js/libs/draco/gltf/' );
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader( dracoLoader );
+loader.load( '/models/littlest_tokyo/glb/LittlestTokyo.glb', function ( gltf ) {
+// loader.load( '/models/APEX_watton/gltf/Wattson_Strange_Attractor.gltf', function ( gltf ) {
+
+    const model = gltf.scene;
+    model.position.set( 1, 1, 0 );
+    model.scale.set( 0.01, 0.01, 0.01 );
+    scene.add( model );
+
+    mixer = new THREE.AnimationMixer( model );
+    mixer.clipAction( gltf.animations[ 0 ] ).play();
+
+    animate();
+
+}, undefined, function ( e ) {
+
+    console.error( e );
+
+} );
+
+
+window.onresize = function () {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    const delta = clock.getDelta();
+
+    mixer.update( delta );
+
+    controls.update();
+
+    stats.update();
+
+    renderer.render( scene, camera );
+
+}
+
